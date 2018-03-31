18a6af8d936fa736b35ccce26aed7e2a677a9b06
Elliot Forbes <emforce@live.com>
2018-03-31 11:15:03 +0100

    Updated readme

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 Vue-C3 Charts
 ===============
 
+Vue-c3 is a node module that makes working with the c3 graphs library a little simpler and allowing you to treat these charts are vue-like components.
+
+## Installation
+
+```
+// Using npm
+npm install vue-c3
+// Using Yarn
+yarn add vue-c3
+```
+
+## Examples
+
+Below you will find some example usage:
+
+### Bar Chart
+
+```html
+<bar-chart v-bind:data="data"></bar-chart>
+```
--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -1,9 +1,5 @@
 <template>
-  <div>
-      <h2>Bar Chart</h2>
-      <button v-on:click="show">Show</button>
-      <div id="chart"></div>
-  </div>
+  <div id="chart"></div>
 </template>
 
 <script>
@@ -11,20 +7,15 @@ import c3 from 'c3'
 
 export default {
   name: 'BarChart',
-  data: function () {
-    return {
-      chartData: {
-        columns: [
-          ['data1', 300, 100, 250, 150, 300, 150, 500],
-          ['data2', 100, 200, 150, 50, 100, 250]
-        ]
-      }
-    }
-  },
+  props: [
+    'data',
+    'axes',
+    'axis'
+  ],
   mounted: function () {
     c3.generate({
       bindto: '#chart',
-      data: this.chartData
+      data: this.data
     })
   }
 }
